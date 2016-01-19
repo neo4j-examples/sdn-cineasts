f94ea849bce84a8f8ed33cf5852beae31ee18479
Luanne Misquitta <luanne.coutinho@gmail.com>
2016-01-19 11:35:33 +0530

    License update

--- src/main/java/org/neo4j/cineasts/domain/User.java
+++ src/main/java/org/neo4j/cineasts/domain/User.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright [2011-2016] "Neo Technology"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
+
 package org.neo4j.cineasts.domain;
 
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.neo4j.cineasts.converter.UserRolesConverter;
 import org.neo4j.ogm.annotation.GraphId;
@@ -8,9 +22,6 @@ import org.neo4j.ogm.annotation.Relationship;
 import org.neo4j.ogm.annotation.typeconversion.Convert;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.core.GrantedAuthority;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @NodeEntity
 public class User {
--- src/main/java/org/neo4j/cineasts/service/DatabasePopulator.java
+++ src/main/java/org/neo4j/cineasts/service/DatabasePopulator.java
@@ -1,4 +1,19 @@
+/*
+ * Copyright [2011-2016] "Neo Technology"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
 package org.neo4j.cineasts.service;
+
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.neo4j.cineasts.domain.Movie;
 import org.neo4j.cineasts.domain.User;
@@ -10,11 +25,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 /**
  * @author mh
