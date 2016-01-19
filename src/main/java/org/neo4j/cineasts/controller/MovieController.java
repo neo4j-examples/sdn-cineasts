06ef0c8a5dab984d09da93c5f37f6e6072727863
Luanne Misquitta <luanne.coutinho@gmail.com>
2016-01-19 11:32:05 +0530

    License update

--- src/main/java/org/neo4j/cineasts/controller/MovieController.java
+++ src/main/java/org/neo4j/cineasts/controller/MovieController.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright [2011-2016] "Neo Technology"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
 package org.neo4j.cineasts.controller;
 
 import java.util.Collection;
@@ -19,7 +29,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author mh
--- src/main/java/org/neo4j/cineasts/movieimport/MovieDbImportService.java
+++ src/main/java/org/neo4j/cineasts/movieimport/MovieDbImportService.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright [2011-2016] "Neo Technology"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
+ */
 package org.neo4j.cineasts.movieimport;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.neo4j.cineasts.domain.*;
+import org.neo4j.cineasts.domain.Actor;
+import org.neo4j.cineasts.domain.Director;
+import org.neo4j.cineasts.domain.Movie;
+import org.neo4j.cineasts.domain.Person;
+import org.neo4j.cineasts.domain.Role;
+import org.neo4j.cineasts.domain.Roles;
 import org.neo4j.cineasts.repository.ActorRepository;
 import org.neo4j.cineasts.repository.DirectorRepository;
 import org.neo4j.cineasts.repository.MovieRepository;
